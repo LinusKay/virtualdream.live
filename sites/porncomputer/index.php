e0e158bee1a17b034cd7bbc62662d66a7e595ace
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2023-08-26 00:40:05 +1000

    Add porncomputer website

---
+++ sites/porncomputer/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORN COMPUTER - WORLD FIRST</title>
+    <style> 
+        html {
+            background-color:antiquewhite;
+        }
+        body {
+            max-width:550px;
+            margin:auto;
+            font-size:18px;
+        }
+        img {
+            width:100%;
+            display:block;
+            margin:auto;
+        }
+        p{
+            width:100%;
+            text-align: center;
+        }
+        .caption {
+            color:gray;
+            margin-top:0;
+            font-style: italic;
+        }
+        #adspace {
+            margin-top:25px;
+        }
+        .advertisement {
+            border:none;
+            display:block;
+            margin:auto;
+        }
+    </style>
+</head>
+<body>
+    <p>I HAVE INTENDED THE WORLDS FIRST computer for PORN ONLY</p>
+
+    <img src="IMG/computer.jpg">
+    <p class="caption">THE COMPUTER</p>
+
+    <p>ONLY WORKS WITH PORN</p>
+    <p>DO NOT TRY TO SEE ANYTHING ELSE IT WILL <i>NOT</i> WORK</p>
+    <p>IF YOU TRY TO SEE ANYTHING ELSE IT WILL <i>BREAK</i></p>
+
+    <img src="IMG/computer2.jpg">
+    <p class="caption">THE COMPUTER</p>
+
+    <p>I SPENT FIFTEEN YEARS AND ALMOST ALL OF MY LIFE SAVINGS DEVELOPING THIS</p>
+    <p>I SPENT THE REMAINING MONEY ON THIS WEBSITE</p>
+    <p>I MUST GET THIS OFF MY HANDS</p>
+
+
+    <p>I BUILT THIS IN MY WORKSHOP USING <i>THESE</i>TOOLS</p>
+
+    <img src="IMG/Tools.jpg">
+    <p class="caption">THE TOOLS</p>
+
+    <p>THESE TOOLS BELONGED TO MY GRANDFATHER</p>
+    <p>HE DIDNT EVEN KNOW WHAT PORN IS</p>
+    <p>AND YOU KNOW WHAT?</p>
+    <p>HE’S DEAD</p>
+    
+    
+    <hr>
+    <p>I USED <i>MY</i> DEAD GRANDDADS <i>OLD</i> TOOLS TO MAKE THE COMPUTER WHERE <i>YOU</i> CAN WATCH PEOPLE <i>BONE</i>. <i><strong>FOREVER</strong></i></p>
+    <hr>
+
+    <p>MY GRANDFATHER WAS AN EVIL MAN WITH WEIRD TOOLS</p>
+    <p>I NEVER EVEN SAW HIM EVEN MAKE ANYTHING AT ALL</p>
+    <p>I DONT KNOW WHAT THIS THING IS</p>
+
+    <img src="IMG/s-l1600 (4).jpg">
+    <p class="caption">I DONT KNOW WHAT IT DOES</p>
+
+    <p>I DONT EVEN LIKE COMPUTER ANYMORE</p>
+    <p>I DONT KNOW HOW TO USE IT I HAVE TO ASK MY SON</p>
+    <p>I USE TO LOOOVE COMPUTER </p>
+    <p>I USED COMPUTER EVERY DAY TO MAKE IMAGES JUST LIKE THIS</p>
+
+    <img src="IMG/mustang.png">
+
+    <p>AND THIS</p>
+
+    <img src="IMG/Telly Robot.png">
+
+    <p>BUT THIS MACHINE HAS SUCKED THE LIFE OUT OF MY VERY BEING FROM HEAD TO TOE AND I DONT KNOW HOW TO GO ON ANYMORE AFTER DOING WHAT IVE DONE</p>
+    <p>I DONT EVEN HAVE A JOB ANYMORE</p>
+    <p>THEY FIRED ME WHEN I SAID I NEEDED MORE TIME TO WORK ON THE PORN COMPUTER</p>
+    <p>THEY <i>HATED</i> MY IDEAS</p>
+        
+    <hr>
+    <p>THEY TOLD ME IT WAS DUMB TO MAKE A COMPUTER THAT WHERE YOU CAN ONLY SIT AND WATCH GUYS BONE INSIDE OF IT </p>
+    <hr>
+
+    <p>THEY SAID THATS A DUMB IDEA</p>
+    <p><strong>BUT YOU DONT <i>HAVE</i> TO JUST WATCH GUYS BONE ON IT <i>THATS THE POINT</i></strong></p>
+    <p>YOU CAN WATCH WHATEVER KIND OF PORN YOU <i>WANT TO</i> I DONT <i>CARE</i> THATS THE <i>POINT</i></p>
+    <p>I DONT CARE WHAT YOU WATCH ON IT </p>
+    <p>ITS NONE OF MY BUSINESS</p>
+    <p>BUT IF ITS <i>NOT</i> PORN AND YOU <i>BREAK</i> THE COMPUTER THEN THATS TOO BAD </p>
+    <hr>
+    <p><strong>OTHER THINGS I HAVE MADE</strong></p>
+
+    <img src="IMG/Phone.jpg">
+
+    <p class="caption">PHONE TO LISTEN TO PORN</p>
+
+    <hr>
+    <p>EMAIL ME AT PORNCOMPUTER@VIRTUALDREAM.LIVE</p>
+    <hr>
+
+    <div id="adspace">
+        <iframe class="advertisement" width="468px" height="55px" style="-webkit-transform:scale(1);-moz-transform-scale(1);" src="https://advertising.virtualdream.live/banner/" scrolling="no"></iframe>
+    </div>
+    <br>
+</body>
+</html>
