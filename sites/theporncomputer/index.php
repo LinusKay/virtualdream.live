b60cb1612e8473f8f1e907816fc2339f572f7df4
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2023-08-30 16:07:58 +1000

    Add additional advertisements, restyle websites

--- sites/theporncomputer/index.php
+++ sites/theporncomputer/index.php
@@ -5,14 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PORN COMPUTER - WORLD FIRST</title>
     <style> 
-        html {
-            background-color:antiquewhite;
-        }
         body {
+            background-color:#004b7f;
+        }
+        #wrap {
             max-width:550px;
             margin:auto;
             font-size:18px;
             text-align: center;
+            border: dashed 1px white;
+            padding:15px;
+            background-color:#0069b3;
         }
         img {
             width:100%;
@@ -20,7 +23,7 @@
             margin:auto;
         }
         .caption {
-            color:gray;
+            opacity:0.5;
             margin-top:0;
             font-style: italic;
         }
@@ -28,161 +31,212 @@
             margin-top:25px;
             text-align:center;
         }
+        .ad-title {
+            font-size:10px;
+            text-align:left;
+            float:left;
+            color:gray;
+        }
         .advertisement {
             border:none;
             display:block;
             margin:auto;
         }
+        #adspace-fixed {
+            position:fixed;
+            bottom:0;
+            z-index:50;
+        }
         .spacer {
-            height:75px;
+            height:5px;
         }
         p{
             margin:30px 0;
         }
+
     </style>
 </head>
 <body>
-    <h2>THE CULMINATION OF MY LIFE'S WORK</h2>
-    <p>I HAVE INVENTED...</p>
+    <div id="wrap">
+        <!-- 
+        ...............ssss.........
+        ..............SSSSSS........
+        ..sexy........|0|03Ss.......
+        .......__.....\ U /Ss.......
+        ....../  \...._| |sSs.......
+        ._.../ /\ \__/_ _  \s.......
+        |_\_/ /__\        \ \s......
+        /____/....\______/ { }......
+        ............................
+        -->
+        <h2>THE CULMINATION OF MY LIFE'S WORK</h2>
+        <p>I HAVE INVENTED....</p>
 
-    <div class="spacer"></div>
+        <div class="spacer"></div>
 
-    <p>THE WORLDS FIRST COMPUTER FOR PORN ONLY</p>
-    <div class="spacer"></div>
+        <p>THE WORLDS FIRST COMPUTER FOR PORN ONLY.</p>
+        <div class="spacer"></div>
 
-    <img src="IMG/computer.jpg">
-    <p class="caption">THE COMPUTER</p>
+        <img src="IMG/computer.jpg">
+        <p class="caption">THE COMPUTER.</p>
 
-    <div id="adspace">
-        <!-- banner advertisement -->
-        <?php include("../advertising/getbanner.php"); ?>
+        <div id="adspace">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getbanner.php"); ?>
+        </div>
+
+        <div class="spacer"></div>
+
+        <p>ONLY WORKS WITH PORN.</p>
+        <p>DO NOT TRY TO SEE ANYTHING ELSE IT WILL <i>NOT</i> WORK.</p>
+        <p>IF YOU TRY TO SEE ANYTHING ELSE IT <u>WILL</u> <i>BREAK</i>.</p>
+
+        <div class="spacer"></div>
+
+        <img src="IMG/computer2.jpg">
+        <p class="caption">THE COMPUTER.</p>
+
+        <div class="spacer"></div>
+
+        <p>I SPENT FIFTEEN YEARS AND ALMOST ALL OF MY LIFE SAVINGS DEVELOPING THIS.</p>
+        <p>I SPENT THE REMAINING MONEY ON THIS WEBSITE.</p>
+
+        <div class="spacer"></div>
+
+        <p>I MUST GET THIS OFF MY HANDS.</p>
+
+        <div class="spacer"></div>
+        
+        <div id="adspace">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getbanner.php"); ?>
+        </div>
+
+        <div class="spacer"></div>
+
+        <p>I BUILT THIS IN MY WORKSHOP USING <i>THESE</i>TOOLS.</p>
+
+        <img src="IMG/Tools.jpg">
+        <p class="caption">THE TOOLS.</p>
+
+        <p>THESE TOOLS BELONGED TO MY GRANDFATHER.</p>
+        <p>HE DIDNT EVEN KNOW WHAT PORN IS.</p>
+        <p>AND YOU KNOW WHAT?.</p>
+
+        <div class="spacer"></div>
+
+        <p><u>HE’S DEAD</u>.</p>
+        
+        <div id="adspace">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getbanner.php"); ?>
+        </div>
+
+        <div class="spacer"></div>
+
+        <hr>
+        <p>I USED <i>MY</i> DEAD GRANDDADS <i>OLD</i> TOOLS TO MAKE THE COMPUTER WHERE <i>YOU</i> CAN WATCH PEOPLE <i>BONE</i>. <i><strong>FOREVER</strong></i>.</p>
+        <hr>
+
+        <div class="spacer"></div>
+
+        <p>MY GRANDFATHER WAS AN EVIL MAN WITH WEIRD TOOLS.</p>
+        <p>I NEVER EVEN SAW HIM EVEN MAKE ANYTHING AT ALL.</p>
+        <p>I DONT KNOW WHAT THIS THING IS.</p>
+
+        <img src="IMG/s-l1600 (4).jpg">
+        <p class="caption">I DONT KNOW WHAT IT DOES.</p>
+
+        <p>I DONT EVEN LIKE COMPUTER ANYMORE.</p>
+        <p>I DONT KNOW HOW TO USE IT I HAVE TO ASK MY SON.</p>
+        <p>I USE TO LOVE COMPUTER .</p>
+        <p>I USED COMPUTER EVERY DAY TO MAKE IMAGES JUST LIKE THIS.</p>
+
+        <img src="IMG/mustang.png">
+
+        <p>AND THIS.</p>
+
+        <img src="IMG/Telly Robot.png">
+
+        <p>BUT THIS MACHINE HAS SUCKED THE LIFE OUT OF MY VERY BEING FROM HEAD TO TOE AND I DONT KNOW HOW TO GO ON ANYMORE AFTER DOING WHAT IVE DONE.</p>
+
+        <div class="spacer"></div>
+
+        <p>I DONT EVEN HAVE A JOB ANYMORE.</p>
+
+        <div class="spacer"></div>
+
+        <p>THEY FIRED ME WHEN I SAID I NEEDED MORE TIME TO WORK ON THE PORN COMPUTER.</p>
+        <p>THEY <i>HATED</i> MY IDEAS.</p>
+                
+        <div id="adspace">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getbanner.php"); ?>
+        </div>
+
+        <div class="spacer"></div>
+
+        <hr>
+        <p>THEY TOLD ME IT WAS DUMB TO MAKE A COMPUTER THAT WHERE YOU CAN ONLY SIT AND WATCH GUYS BONE INSIDE OF IT .</p>
+        <hr>
+
+        <p>THEY SAID THATS A DUMB IDEA.</p>
+
+        <div class="spacer"></div>
+
+        <p><strong>BUT YOU DONT <i>HAVE</i> TO JUST WATCH GUYS BONE ON IT <i>THATS THE POINT</i></strong>.</p>
+        <p>YOU CAN WATCH WHATEVER KIND OF PORN YOU <i>WANT TO</i> I DONT <i>CARE</i> THATS THE <i>POINT</i>.</p>
+        <p>I DONT CARE WHAT YOU WATCH ON IT .</p>
+        <p>ITS NONE OF MY BUSINESS.</p>
+
+        <div class="spacer"></div>
+
+        <p>BUT IF ITS <i>NOT</i> PORN AND YOU <i>BREAK</i> THE COMPUTER THEN THATS TOO BAD .</p>
+        
+        <div class="spacer"></div>
+
+        <hr>
+        <p><strong>OTHER THINGS I HAVE MADE</strong>.</p>
+
+        <img src="IMG/Phone.jpg">
+
+        <p class="caption">PHONE TO LISTEN TO PORN.</p>
+
+        <hr>
+        <p>EMAIL ME AT PORNCOMPUTER@VIRTUALDREAM.LIVE.</p>
+        <hr>
+
+        <div id="adspace">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getbanner.php"); ?>
+        </div>
+        <!--
+        .............................
+        ........______________.......
+        ......./ ________  \  \_.....
+        ......| |        | |    \....
+        ......| |  ;)    | |    |....
+        ......| |________| |    |....
+        .......\___________/___/.....
+        ......./___________\__ \.....
+        ..../ QWERTYUIOP  / /   |....
+        .../ ASDFGHJKL;' / /    |....
+        ../ ZXCVBNM,.?  /_/_____|....
+        .............................
+        .............................
+        -->
+        
+        <br>
     </div>
-
-    <div class="spacer"></div>
-
-    <p>ONLY WORKS WITH PORN</p>
-    <p>DO NOT TRY TO SEE ANYTHING ELSE IT WILL <i>NOT</i> WORK</p>
-    <p>IF YOU TRY TO SEE ANYTHING ELSE IT WILL <i>BREAK</i></p>
-
-    <div class="spacer"></div>
-
-    <img src="IMG/computer2.jpg">
-    <p class="caption">THE COMPUTER</p>
-
-    <div class="spacer"></div>
-
-    <p>I SPENT FIFTEEN YEARS AND ALMOST ALL OF MY LIFE SAVINGS DEVELOPING THIS</p>
-    <p>I SPENT THE REMAINING MONEY ON THIS WEBSITE</p>
-
-    <div class="spacer"></div>
-
-    <p>I MUST GET THIS OFF MY HANDS</p>
-
-    <div class="spacer"></div>
-    
-    <div id="adspace">
-        <!-- banner advertisement -->
-        <?php include("../advertising/getbanner.php"); ?>
-    </div>
-
-    <div class="spacer"></div>
-
-    <p>I BUILT THIS IN MY WORKSHOP USING <i>THESE</i>TOOLS</p>
-
-    <img src="IMG/Tools.jpg">
-    <p class="caption">THE TOOLS</p>
-
-    <p>THESE TOOLS BELONGED TO MY GRANDFATHER</p>
-    <p>HE DIDNT EVEN KNOW WHAT PORN IS</p>
-    <p>AND YOU KNOW WHAT?</p>
-
-    <div class="spacer"></div>
-
-    <p>HE’S DEAD</p>
-    
-    <div id="adspace">
-        <!-- banner advertisement -->
-        <?php include("../advertising/getbanner.php"); ?>
-    </div>
-
-    <div class="spacer"></div>
-
-    <hr>
-    <p>I USED <i>MY</i> DEAD GRANDDADS <i>OLD</i> TOOLS TO MAKE THE COMPUTER WHERE <i>YOU</i> CAN WATCH PEOPLE <i>BONE</i>. <i><strong>FOREVER</strong></i></p>
-    <hr>
-
-    <div class="spacer"></div>
-
-    <p>MY GRANDFATHER WAS AN EVIL MAN WITH WEIRD TOOLS</p>
-    <p>I NEVER EVEN SAW HIM EVEN MAKE ANYTHING AT ALL</p>
-    <p>I DONT KNOW WHAT THIS THING IS</p>
-
-    <img src="IMG/s-l1600 (4).jpg">
-    <p class="caption">I DONT KNOW WHAT IT DOES</p>
-
-    <p>I DONT EVEN LIKE COMPUTER ANYMORE</p>
-    <p>I DONT KNOW HOW TO USE IT I HAVE TO ASK MY SON</p>
-    <p>I USE TO LOVE COMPUTER </p>
-    <p>I USED COMPUTER EVERY DAY TO MAKE IMAGES JUST LIKE THIS</p>
-
-    <img src="IMG/mustang.png">
-
-    <p>AND THIS</p>
-
-    <img src="IMG/Telly Robot.png">
-
-    <p>BUT THIS MACHINE HAS SUCKED THE LIFE OUT OF MY VERY BEING FROM HEAD TO TOE AND I DONT KNOW HOW TO GO ON ANYMORE AFTER DOING WHAT IVE DONE</p>
-
-    <div class="spacer"></div>
-
-    <p>I DONT EVEN HAVE A JOB ANYMORE</p>
-
-    <div class="spacer"></div>
-
-    <p>THEY FIRED ME WHEN I SAID I NEEDED MORE TIME TO WORK ON THE PORN COMPUTER</p>
-    <p>THEY <i>HATED</i> MY IDEAS</p>
-            
-    <div id="adspace">
-        <!-- banner advertisement -->
-        <?php include("../advertising/getbanner.php"); ?>
-    </div>
-
-    <div class="spacer"></div>
-
-    <hr>
-    <p>THEY TOLD ME IT WAS DUMB TO MAKE A COMPUTER THAT WHERE YOU CAN ONLY SIT AND WATCH GUYS BONE INSIDE OF IT </p>
-    <hr>
-
-    <p>THEY SAID THATS A DUMB IDEA</p>
-
-    <div class="spacer"></div>
-
-    <p><strong>BUT YOU DONT <i>HAVE</i> TO JUST WATCH GUYS BONE ON IT <i>THATS THE POINT</i></strong></p>
-    <p>YOU CAN WATCH WHATEVER KIND OF PORN YOU <i>WANT TO</i> I DONT <i>CARE</i> THATS THE <i>POINT</i></p>
-    <p>I DONT CARE WHAT YOU WATCH ON IT </p>
-    <p>ITS NONE OF MY BUSINESS</p>
-
-    <div class="spacer"></div>
-
-    <p>BUT IF ITS <i>NOT</i> PORN AND YOU <i>BREAK</i> THE COMPUTER THEN THATS TOO BAD </p>
-    
-    <div class="spacer"></div>
-
-    <hr>
-    <p><strong>OTHER THINGS I HAVE MADE</strong></p>
-
-    <img src="IMG/Phone.jpg">
-
-    <p class="caption">PHONE TO LISTEN TO PORN</p>
-
-    <hr>
-    <p>EMAIL ME AT PORNCOMPUTER@VIRTUALDREAM.LIVE</p>
-    <hr>
-
-    <div id="adspace">
-        <!-- banner advertisement -->
-        <?php include("../advertising/getbanner.php"); ?>
-    </div>
-    <br>
+    <div id="adspace-fixed">
+            <!-- Banner advertisement -->
+            <span class="ad-title">Advertisement</span>
+            <?php include("../advertising/getcard.php"); ?>
+        </div>
 </body>
 </html>
