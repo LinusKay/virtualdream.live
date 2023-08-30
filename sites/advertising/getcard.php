c012fc570195ba143fa912ce7747de6e4dc5d344
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2023-08-30 22:36:20 +1000

    Set advert image src to pull from live site as workaround, add base files for cursedwares

--- sites/advertising/getcard.php
+++ sites/advertising/getcard.php
@@ -12,5 +12,5 @@
     $ad = $adverts[$ad_selected];
     $ad_url = $ad[0];
     $ad_img = $ad[1];
-    echo "<a class=\"advertisement-card\" href=\"$ad_url\"><img src=\"../advertising/card/images/$ad_img\"></a>";
+    echo "<a class=\"advertisement-card\" href=\"$ad_url\"><img src=\"https://advertising.virtualdream.live/card/images/$ad_img\"></a>";
 ?>
