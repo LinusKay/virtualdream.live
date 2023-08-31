6f61d3c8567bae45c5b6154046bd770423a5377d
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2023-08-31 21:49:22 +1000

    Add ratparty

---
+++ sites/ratparty/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" style="background: url('balloons_tile.png');background-repeat: repeat;font-family:cursive;color:lime;text-decoration:underline;">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>~rat party~</title>
+</head>
+<body style="width:700px;margin:auto;text-align:center;background-color:aliceblue;">
+<h1>rat party</h1>
+<img src="0vwwiysamtu61.jpg" style="width:500px;">
+<br>
+<audio controls autoplay loop>
+<source src="ratparty.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio> 
+<br>
+<p>visitors:<a href="https://www.hitwebcounter.com" target="_blank">
+<img src="https://hitwebcounter.com/counter/counter.php?page=8933982&style=0006&nbdigits=5&type=page&initCount=97041" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></a>           
+</p>
+<img src="giantratanim.gif" style="display:inline-block;width:50px;">
+
+</body>
+</html>
