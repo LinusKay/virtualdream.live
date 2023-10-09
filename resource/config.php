2bd60000605dd4f789984984aba2bec0b2f3836d
Linus Kay <s3668218@student.rmit.edu.au>
2023-10-10 10:33:05 +1100

    Define ROOT directory in PHP

---
+++ resource/config.php
@@ -0,0 +1,1 @@
+<?php define("ROOT", __DIR__ ."/"); ?>
--- sites/gobingo/index.php
+++ sites/gobingo/index.php
@@ -1,4 +1,9 @@
 <?php include('../../resource/header.php'); ?>
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
