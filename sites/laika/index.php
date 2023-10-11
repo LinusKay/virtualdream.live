72a522724871d8679d70e76c6d16caefcb277995
Linus Kay <s3668218@student.rmit.edu.au>
2023-10-11 15:52:09 +1100

    Add favicon and title to laika

--- sites/laika/index.php
+++ sites/laika/index.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="src/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="src/favicon/favicon-16x16.png">
+    <link rel="manifest" href="src/favicon/site.webmanifest">
+    <title>✧˖°.</title>
     <style>
         @font-face {
             font-family: "amstrad";
