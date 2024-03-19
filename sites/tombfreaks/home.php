255548ff9f15aaac4c0ac279c6aaa3cff8d8e264
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-03-19 16:22:15 +1100

    Additions to tombfreaks homepage

--- sites/tombfreaks/home.php
+++ sites/tombfreaks/home.php
@@ -37,39 +37,37 @@
         <table align="center" cellpadding="20" height="395" width="820" bgcolor="#000000" style="color:white;">
             <tbody>
                 <tr>
-                    <td>logo</td>
-                </tr>
-                <tr>
                     <td align="left" width="30%">
-                        <img src="src/img/headstone.png" style="display:block;margin:auto;" align="center">
                         <ul style="list-style-type:none">
-                            <li><a href="bigfoot"><b>bigfoot</b></a></li>
-                            <li><a href="bigfoot"><b>ufo</b></a></li>
-                            <li><a href="bigfoot"><b>vampires</b></a></li>
-                            <li><a href="bigfoot"><b>goblins</b></a></li>
-                            <li><a href="bigfoot"><b>cryptids</b></a></li>
+                            <li><a href="bigfoot.php"><b>bigfoot</b></a></li>
+                            <li><a href="ufo.php"><b>ufo</b></a></li>
+                            <li><a href="vampires.php"><b>vampires</b></a></li>
+                            <li><a href="goblins.php"><b>goblins</b></a></li>
+                            <li><a href="cryptids.php"><b>cryptids</b></a></li>
                             <li><a href="mailto:tombfreaks@virtualdream.live">EMAIL US</a></li>
                         </ul>
                         <img src="src/img/usa.gif" width="100" style="display:block;margin:auto">
+                        <br>
+                        <div class="advertisement-card"></div>
                     </td>
                     <td align="left" style="color:red">
-                    the cryptkeeper is away... come in, come in...
-                    <br>
-                    crawling into bed after a long day you hear a creek<br>
-                    laying awake in the dark your eyes scan the corners and see... nothing<br>
-                    laying your head back down you hear it again, and again...<br>
-                    <br>
-                    welcome to the tomb
-
-                    <br>
-                    <div class="advertisement-banner"></div>
-                    <br>
-                    more spooky stuff...
-                    <br>
-                    <div class="advertisement-card"></div>
-                    <br>
-                    we are supported by ads
-                </td>
+                    <img src="src/img/logo-export.png" style="display:block;margin:auto;">
+                        the cryptkeeper is away... come in, come in...
+                        <br>
+                        crawling into bed after a long day you hear a creek<br>
+                        laying awake in the dark your eyes scan the corners and see... nothing<br>
+                        laying your head back down you hear it again, and again...<br>
+                        <br>
+                        <img src="src/img/skull.png" style="display:block;margin:auto">
+                        <br>
+                        welcome to the tomb
+                        <br>
+                        <img src="src/img/boneline.gif">
+                        <div class="advertisement-banner"></div>
+                        <img src="src/img/boneline.gif">
+                        <br>
+                        tombfreaks@virtualdream.live
+                    </td>
                 </tr>
             </tbody>
         </table>
