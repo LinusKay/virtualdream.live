870c618e0dc3b1f94772bf7defef6faeb4c2f88f
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-05-23 18:01:54 +1000

    Add meta tags to zambonisimulator

--- sites/zambonisimulator/index.php
+++ sites/zambonisimulator/index.php
@@ -4,6 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
+    
+    <title>Zamboni Simulator — Unleash the Freeze</title>
+    <meta name="title" content="Zamboni Simulator — Unleash the Freeze" />
+    <meta name="description" content="Step into the rawest ice rink experience online. No bloat. No microtransactions. No special features. Just pure ice polishing experience. Game on!" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://virtualdream.live/sites/zambonisimulator/" />
+    <meta property="og:title" content="Zamboni Simulator — Unleash the Freeze" />
+    <meta property="og:description" content="Step into the rawest ice rink experience online. No bloat. No microtransactions. No special features. Just pure ice polishing experience. Game on!" />
+    <meta property="og:image" content="https://virtualdream.live/sites/zambonisimulator/zambonisimulator/zambonisimulator/splash.png" />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://virtualdream.live/sites/zambonisimulator/" />
+    <meta property="twitter:title" content="Zamboni Simulator — Unleash the Freeze" />
+    <meta property="twitter:description" content="Step into the rawest ice rink experience online. No bloat. No microtransactions. No special features. Just pure ice polishing experience. Game on!" />
+    <meta property="twitter:image" content="https://virtualdream.live/sites/zambonisimulator/zambonisimulator/zambonisimulator/splash.png" />
     <?php 
     # PAGE SETUP
     $screensaverOptOut = true;
