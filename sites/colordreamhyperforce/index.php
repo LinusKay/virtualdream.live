85005c267dac66960886d9cad7e9b98fd5d34f16
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-06-08 22:56:03 +1000

    Add more colours to colordreamhyperforce

--- sites/colordreamhyperforce/index.php
+++ sites/colordreamhyperforce/index.php
@@ -38,38 +38,73 @@
 <p>I was at home on another night skunked up at home on the couch, exploring my mind through the fruits of cherished night-time sci-fi marathon television. I hungered for another pound of Nubson's Chocolate-filled Cherry Blast Bitlets. Making the journey across my exotic and finely-rugged apartment, passing the signed, framed portrait of my beautiful Xebulon, the warrior princess of Xarxax VII, I was hit with a wave of love or a cosmic ray or a moon ray, and I fell over. I don't know what happened next, or how long I was out, but when I woke, my head was stuck all the way inside that microwave with settings to HIGH. It took all of my roommates 2 hours to wrench my head free from the machine that had me so tight in its grasp.</p>
 <p>But ever since the fated incident I have never slept the same. Like the lights of Xebulon and her divine cosmic rays, the colors blast my eyes ass I drift into the night. It was at this moment I realised I was born for something greater. The calling I had been waiting for that I knew would appear when I needed it the most was finally here. </p>
 <p>During my sleep sessions I have seen colors you couldn’t even imagine. I've already begun naming them:</p>
-<ul>
-    <li>Hyper-purple</li>
-    <li>Globulax’s Green</li>
-    <li>Bitlet Blast Blue</li>
-    <li>Hrorange</li>
-    <li>Redd</li>
-    <li>Dream Five</li>
-    <li>Kthinxktakt</li>
-    <li>Xarxax Xenon</li>
-    <li>Xebulon Black</li>
-    <li>Glue (mix of green and blue)</li>
-    <li>Robometal Red</li>
-    <li>Grimetown Green</li>
-    <li>Chanlo</li>
-    <li>Yellue</li>
-    <li>Cosmic Brown</li>
-    <li>Space Holiday</li>
-    <li>Xebulon Eye</li>
-    <li>Unseeable Orange</li>
-    <li>Invisible Number Seven</li>
-    <li>Space-Goblin Hue</li>
-    <li>Xebulon Lips</li>
-    <li>Nightmare White</li>
-    <li>Space-void Brown</li>
-    <li>Bleen (mix of blue and green)</li>
-    <li>Unholy Silver</li>
-    <li>grown</li>
-    <li>Thumbtack</li>
-    <li>Groinge</li>
-    <li>Kthumgar Kthyellow</li>
-    <li>Rainbow Green</li>
-</ul>
+<table>
+    <tbody>
+        <tr>
+            <td>
+                <ul>
+                    <li>Hyper-purple</li>
+                    <li>Globulax’s Green</li>
+                    <li>Bitlet Blast Blue</li>
+                    <li>Hrorange</li>
+                    <li>Redd</li>
+                    <li>Dream Five</li>
+                    <li>Kthinxktakt</li>
+                    <li>Xarxax Xenon</li>
+                    <li>Xebulon Black</li>
+                    <li>Glue (mix of green and blue)</li>
+                    <li>Robometal Red</li>
+                    <li>Grimetown Green</li>
+                    <li>Chanlo</li>
+                    <li>Yellue</li>
+                    <li>Cosmic Brown</li>
+                    <li>Space Holiday</li>
+                    <li>Xebulon Eye</li>
+                    <li>Unseeable Orange</li>
+                    <li>Invisible Number Seven</li>
+                    <li>Space-Goblin Hue</li>
+                    <li>Xebulon Lips</li>
+                    <li>Nightmare White</li>
+                    <li>Space-void Brown</li>
+                    <li>Bleen (mix of blue and green)</li>
+                    <li>Unholy Silver</li>
+                    <li>grown</li>
+                    <li>Thumbtack</li>
+                    <li>Groinge</li>
+                    <li>Kthumgar Kthyellow</li>
+                    <li>Rainbow Green</li>
+                </ul>
+            </td>
+            <td>
+            <ul>
+                <li>Cosmic Hypernova</li>
+                <li>Xarxax Quirkquartz</li>
+                <li>Electronic Red</li>
+                <li>Blobulax</li>
+                <li>Gravity Green</li>
+                <li>Xebulon Hair</li>
+                <li>Quasarnk</li>
+                <li>Antimatter</li>
+                <li>Cosmic Radioactive Yellow</li>
+                <li>Background Noise</li>
+                <li>Captain Battery Yellow</li>
+                <li>Large Don</li>
+                <li>Tommy Soup</li>
+                <li>Insanity Brown</li>
+                <li>Red</li>
+                <li>Cherry Zap</li>
+                <li>Strong Yellow</li>
+                <li>Unbelievable Heat</li>
+                <li>Xarxax Flame</li>
+                <li>Xebulon</li>
+                <li>Heart Pink</li>
+                <li>Courageous Gray</li>
+            </ul>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
 <p>Im no fool. I immediately attempted to patent the colors, to protect the powers with which I have had thrust upon me. But every damn lawyer in the state told me i was crazy and that it would never work. </p>
 <p>thats why i knew i had to do something about it myself. It was time to take matters into my own hands and rend my fate as I knew it to be.</p>
 <p>Over a clandestine meeting I have shared the secret with my five closest roommates and now...</p>
