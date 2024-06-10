37f9aec90afa2e18e71e657b43ab46d3307609ed
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-06-11 00:07:09 +1000

    Add even more consistent sizing to malpal image

--- sites/10000000/index.php
+++ sites/10000000/index.php
@@ -32,32 +32,32 @@
         #poem {
             padding:10px;
         }
-        img {
+        .decor {
             max-height:100px;
         }
     </style>
 </head>
 <body>
     <audio autoplay loop><source src="src/audio/10000000angels.ogg"></audio>
-    <img src="src/img/sign_congratulations_lg_nwm.gif">
+    <img class="decor" src="src/img/sign_congratulations_lg_nwm.gif">
 <h1>Congratulation!</h1>
 <p>For being our 10000000th visitor!</p>
 <p>We've waited so long for you!</p>
 <p style="position:absolute; top:150px; left:230px;">You're finally here!</p>
-<img src="src/img/angel4r.gif" style="position:absolute; top:200px; left:250px; cursor:help;" title="a messenger from God">
+<img class="decor" src="src/img/angel4r.gif" style="position:absolute; top:200px; left:250px; cursor:help;" title="a messenger from God">
 <p style="position:absolute; bottom:150px; right:200px;">Blessings upon you, chosen one</p>
-<img src="src/img/angelCLR.gif" style="position:absolute; bottom:200px; right:230px; cursor:help;" title="blub blub blub">
+<img class="decor" src="src/img/angelCLR.gif" style="position:absolute; bottom:200px; right:230px; cursor:help;" title="blub blub blub">
 <div id="bignumber">
-    <img src="src/img/Angels_Angel_flies_1prv.gif">
-    <img src="src/img/1.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/0.gif">
-    <img src="src/img/Angels_Angel_flies_2_prv.gif">
+    <img class="decor" src="src/img/Angels_Angel_flies_1prv.gif">
+    <img class="decor" src="src/img/1.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/0.gif">
+    <img class="decor" src="src/img/Angels_Angel_flies_2_prv.gif">
 </div>
 <div id="poem">
     <p>A poem, just for you</p>
@@ -65,16 +65,16 @@
     consectetur adipiscing elit, <br>
     sed do eiusmod tempor incididunt <br>
     ut labore et dolore magna aliqua.</p>
-    <img src="src/img/vineflowers.gif" style="width:300px;">
+    <img class="decor" src="src/img/vineflowers.gif" style="width:300px;">
 </div>
-    <img src="src/img/congratulationsCLR.gif" style="cursor:help;" title="drink it up! today is your day!">
-<img src="src/img/angel_blowingkisses.gif" style="position:absolute; top:650px; left:430px; width:150px; cursor:help;" title="we write your story to eternity">
-<img src="src/img/gbook.gif" style="position:absolute; top:715px; left:500px; width:75px; cursor:help;" title="the book of love">
+    <img class="decor" src="src/img/congratulationsCLR.gif" style="cursor:help;" title="drink it up! today is your day!">
+<img class="decor" src="src/img/angel_blowingkisses.gif" style="position:absolute; top:650px; left:430px; width:150px; cursor:help;" title="we write your story to eternity">
+<img class="decor" src="src/img/gbook.gif" style="position:absolute; top:715px; left:500px; width:75px; cursor:help;" title="the book of love">
 <p>All this, and just for you!</p>
-<img src="src/img/i_love_you.gif" style="cursor:help;" title="its never been more true"><br>
-<img src="src/img/heartspin.gif">
-<img src="src/img/dovec.gif" style="position:absolute; top:515px; left:150px; cursor:help;" title="Two doves in love. With You!">
-<img src="src/img/doverigth.gif" style="position:absolute; top:515px; left:200px; cursor:help;" title="Two doves in love. With You!">
+<img class="decor" src="src/img/i_love_you.gif" style="cursor:help;" title="its never been more true"><br>
+<img class="decor" src="src/img/heartspin.gif">
+<img class="decor" src="src/img/dovec.gif" style="position:absolute; top:515px; left:150px; cursor:help;" title="Two doves in love. With You!">
+<img class="decor" src="src/img/doverigth.gif" style="position:absolute; top:515px; left:200px; cursor:help;" title="Two doves in love. With You!">
 <script>
     // alert("YOU'VE DONE IT!! Congratulations!\n\n~~~~~~~~\n\nYou are our 10000000th Visitor!")
 </script>
