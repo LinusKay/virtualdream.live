78bf07fafdf5e90a7903b52750911f3e27be9180
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2024-06-14 15:29:11 +1000

    Update wiptemplate sitename

--- src/include/placeholder/placeholder.php
+++ src/include/placeholder/placeholder.php
@@ -2,7 +2,7 @@
 <center>
 <table width="500" border="0" cellspacing="0" cellpadding="4">
     <tbody>
-        <tr><td colspan="2"><font face="arial" size="+1"><b><?php echo $pageTitle;?>'s Home Page</b></font></td></tr>
+        <tr><td colspan="2"><font face="arial" size="+1"><b><?php echo $siteName;?>'s Home Page</b></font></td></tr>
         <tr>
             <td width="1%"><img src="https://assets.virtualdream.live/img/Computer.gif" border="0" hspace="2" width="41"></td>
             <td>I'm still working on my website. Please check back soon!</td>
@@ -12,7 +12,7 @@
 <hr size="1" width="500">
 <table width="500" border="0" cellspacing="0" cellpadding="4">
     <tbody>
-        <tr><td colspan="2"><font face="arial" size="+1"><b>Do you own <?php echo $pageTitle;?>?</b></font></td></tr>
+        <tr><td colspan="2"><font face="arial" size="+1"><b>Do you own <?php echo $siteName;?>?</b></font></td></tr>
         <tr><td><font face="arial" size="-1">This placeholder home page was created automatically when you signed up for Virtual Dream.</font><td></tr>
         <tr><td><font face="arial" size="-1">Replace this <b>index.php</b> to show off your own content.</font><td></tr>
         <tr><td><font face="arial" size="-1">Want a site of your own? Check out <b><a href="https://virtualdream.live/">Virtual Dream</a></b>!</font><td></tr>
