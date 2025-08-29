fd1d4422177407379df1a6530820bc636e7d1b75
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2025-08-29 22:30:47 +1000

    Add thebutton... finally

---
+++ sites/thebutton/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php 
+        # PAGE SETUP
+        include('../../src/setup.php');
+        # /PAGE SETUP
+        ?>
+        <title>The Button</title>
+        <style> 
+            
+        </style>
+    </head>
+    <body>
+        <center>
+        <p>At the dawn of time, the universe was created.</p>
+        <br>
+        <p>Millenia later, man created fire.</p>
+        <br>
+        <p>All that is secondary as we bring you...</p>
+        <br>
+        <iframe src='https://thebutton.lostpixels.org/embed' style='border-width:0px;width: 224px;height: 130px;'></iframe>
+        <br>
+        <p>behold its crisp, rectangular form</p>
+        <p>breathe in the aroma as dust passes through the PC fans to render it</p>
+        <p>press... if you dare</p>
+        <!-- <div id='thebutton-container' style='position:fixed;bottom:25px;right:25px;margin:0;'> -->
+            
+        <!-- </div> -->
+        </center>
+    </body> 
+</html>
