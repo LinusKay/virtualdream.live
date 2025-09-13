67c72c89ae96996275e558db2c7da561e140ea05
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2025-09-13 13:21:03 +1000

    Removed, added, updated some test files/grahpipcs

---
+++ sites/test/index2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>███ █████</title>
+    <?php 
+        include('../../src/setup.php');
+    ?>
+    <style>
+        body {
+            background: #648fc6;
+            width:600px;
+            margin:auto;
+        }
+        #header {
+            width:600px;
+            height:450px;
+            position:relative;
+        }
+        #header img {
+            width:600px;
+        }
+        #header .navlink {
+            width:80px;
+            height: 35px;
+            display: block;
+            position:absolute;
+            opacity:1;
+            /* background:red; */
+            /* border: dashed 1px white; */
+        }
+        #header .navlink:active {
+            border: dashed 1px darkgray;
+        }
+        #body {
+            color:white;
+            text-align: justify;
+        }
+    </style>
+</head>
+<body bgcolor="#ffffff">
+    <center>
+        <div id="header">
+            <img src="metalheart01_02_ui.png">
+            <a class="navlink" href="" style="top:170px;left:49px;"></a>
+            <a class="navlink" href="" style="top:213px;left:1px;"></a>
+            <a class="navlink" href="" style="top:237px;left:67px;"></a>
+            <a class="navlink" href="" style="top:277px;left:143px;"></a>
+        </div>
+        <div id="body">
+            <p>Labore aliquip non magna officia aute deserunt ipsum laborum duis nostrud do proident consequat sit. Est nulla veniam velit aliqua veniam commodo cillum nulla et veniam non fugiat ullamco officia. Duis ad non in commodo reprehenderit nisi anim aliqua velit. Consectetur ad anim aliquip voluptate deserunt aliqua magna. Est quis consectetur quis enim proident aute tempor do nulla cillum nulla veniam do cillum.</p>
+        </div>
+    </center>
+</body>
+</html>
