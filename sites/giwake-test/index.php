b3a2134c66e5359cca8c260fc70e6aef14f15499
giwake <gabriel.isaac.wake@gmail.com>
2025-09-13 17:46:59 +1000

    Create index.php

---
+++ sites/giwake-test/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Virtual Dream!</title>
+    <?php 
+        include('../../src/setup.php');
+    ?>
+</head>
+<body>
+   <p>POOPIS!</p>
+</body>
+</html>
