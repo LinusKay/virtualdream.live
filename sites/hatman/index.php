59079f753bae27a8504bc1b8cb8f252740ce243e
giwake <gabriel.isaac.wake@gmail.com>
2025-09-15 08:08:43 +1000

    hatman site - haven't tested w/ apache yet

---
+++ sites/hatman/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WHY DID THE ANCIENTS WEAR HATS</title>
+    <?php 
+        include('../../src/setup.php');
+    ?>
+</head>
+
+
+<body background="src/img/tophat1.jpg">
+
+<div align="center">
+  <center>
+
+<table border="1" cellpadding="0" cellspacing="0" style="border-width:0; border-collapse: collapse" bordercolor="#111111" width="50%" bgcolor="#C0C0C0">
+  <tr>
+    <td width="100%" style="border-left-style: none; border-left-width: medium; border-right-style: none; border-right-width: medium; border-top-style: none; border-top-width: medium">
+    <p align="center"><font size="7">WHY DID THE ANCIENTS WEAR HATS?</font></p>
+    <p align="center"><font size="4"><marquee behavior="alternate">THE TRUTH THE HATLESS COWARDS DON'T WANT YOU TO KNOW</marquee></font></td>
+  </tr>
+  <tr>
+    <td width="100%" style="border-left: medium none #111111; border-right-color: #111111; border-right-width: 1; border-top-style: solid; border-top-width: 1; border-bottom-style: none; border-bottom-width: medium">&nbsp;<blockquote>
+      <p align="center"><b><font size="4">WHY ARE OUR POLITICIANS SO TERRIBLE 
+      NOWADAYS? </font></b></p>
+      <p align="center"><b><font size="4">WHY DO SCIENTISTS ONLY PROVE TRIVIA 
+      ANSWERS FOR PUB NIGHT? </font></b></p>
+      <p align="center"><b><font size="4">WHY DO TEACHERS NO LONGER TEACH CURSIVE 
+      AND PROPER MANNERS?</font></b></p>
+      <p align="center"><b><font size="4">MODERN SOCIETY IS FRAUGHT WITH STUPID AND 
+      DUMB ASS PEOPLE AND THE REASON:</font></b></p>
+      <p align="center"><u><b><font size="7" color="#FF0000">NO. HATS.</font></b></u></p>
+      <p align="left"><font size="4">THIS IS WHAT LEADS TO THE DEVIL-OUTION OF 
+      SOCITEY. HATLESS MEN IN CHARGE REVERT BACK TO PRIMORDIAL OOZE OF SATAN 
+      WHILE STRONG HATTED MEN ARE PERSECUTED!</font></p>
+      <p align="left"><font size="4">KNOW-IT-ALLS CLAIM THAT HATS ARE SIMPLY A 
+      FASHION STATEMENT. THEY ARE WONT. WHY ARE THEY STILL AROUND? WHY DID THE 
+      ANCIENTS WEAR THEM FOR CENTURIES? WELL. . .</font></p>
+      <p align="center"><u><b><font size="7">THE TRUTH (NOT &quot;THEORY&quot;)</font></b></u></p>
+      <p align="center">
+      <img border="0" src="src/img/tophat2.jpg" width="302" height="335"></p>
+      <ol>
+        <li>
+        <p align="left"><font size="5">ENERGY LEAVES BODY FROM TOP OF HEAD IN 
+        FORM OF SO-CALLED &quot;HEAT&quot;</font></li>
+        <li>
+        <p align="left"><font size="5">PLACE HAT ON TOP OF HEAD TO CAPTURE 
+        ENERGY</font></li>
+        <li>
+        <p align="left"><font size="5">WITH NOWHERE TO GO THE ENERGY GOES BACK 
+        INSIDE THE HEAD</font></li>
+      </ol>
+      <p align="left"><font size="4">NO ONE HAS BEEN ABLE TO PROVE ME WRONG ON 
+      THIS AT <b><i>ALL</i></b>. EVERY ONE WHO TRIES IS ALWAYS A WEAK SCIENTIST 
+      OR TEACHER WITH A BIAS AGAINST REALITY.</font></p>
+      <p align="left"><font size="4">LET ME ELABORATE ON THIS TRUTH (NOT THEORY) 
+      FOR THOSE TROGLODYTES WHO MAY NOT UNDERSTAND THE SIMPLE, TRUE AND REAL OF 
+      THE HAT.</font></p>
+      <hr>
+      <p align="center"><b><font size="4">STEP 1: ENERGY LEAVES BODY FROM TOP OF 
+      HEAD IN FORM OF SO-CALLED &quot;HEAT&quot;</font></b></p>
+      <p align="left"><font size="4">ENERGY RISES TO THE SKY BECAUSE THAT IS 
+      WHERE IT CAME FROM, IT NATURALLY WANTS TO RETURN &quot;HOME&quot;. (WE GET OUR 
+      ENERGY FROM CONSUMING ANIMALS, WHICH GET THEIR ENERGY FROM PLANTS, WHICH 
+      GET THEIR ENERGY FROM THE SUN. THAT IS THE ENERGY'S HOME.)</font></p>
+      <p align="center"><b><font size="4">STEP </font></b><font size="4"><b>2: 
+      PLACE HAT ON TOP OF HEAD TO CAPTURE ENERGY</b></font></p>
+      <p align="left"><font size="4">THE MOST OPTIMAL KIND OF HAT IS A TOP HAT 
+      AS IT ALLOWS THE ENERGY TO BREATHE BEFORE GOING BACK INSIDE YOUR HEAD. 
+      FAKE &quot;HATS&quot; LIKE BASEBALL CAPS AND BEANIES WILL SUFFOCATE THE ENERGY AND 
+      NOT WORK.</font></p>
+      <p align="center"><b><font size="4">STEP </font></b><font size="4"><b>3: 
+      WITH NOWHERE TO GO THE ENERGY GOES BACK INSIDE THE HEAD</b></font></p>
+      <p align="left"><font size="4">SIMPLE AND CLEAR. THE ENERGY TURNS AROUND 
+      AND ENTERS THE HEAD AGAIN. THIS IS ALSO WHY MANY OTHER FAKE &quot;HATS&quot; WILL 
+      NOT WORK - THEY HAVE HOLES THAT WILL LET THE ENERGY LEAVE. </font></p>
+      <hr>
+      <p align="left"><font size="4"><u><b>THE TOP HAT IS THE MOST OPTIMAL HAT 
+      FOR ENERGY RECYCLING.</b></u> ITS TALL STATURE ALLOWS THE ENERGY TO 
+      BREATHE THE MOST, BUT DOESN'T LET IT OUT. OTHER ACCEPTABLE HATS FROM YEARS 
+      PAST AND NOW INCLUDE:</font></p>
+      <ul>
+        <li>
+      <p align="left"><font size="4"><b>WIZARD HATS</b> (WHY DO YOU THINK THEY 
+      WERE SO SMART)</font></p>
+        </li>
+        <li>
+      <p align="left"><font size="4"><b>CONE HATS</b> (CAN BE CRAFTED AND USED 
+      IN A DIRE SITUATION)</font></p>
+        </li>
+        <li>
+      <p align="left"><font size="4"><b>CHEF'S HAT</b> (GIVES THEM ABILITY TO 
+      COOK GOOD)</font></p>
+        </li>
+        <li>
+      <p align="left"><b><font size="4">CAPOTAIN</font></b></p>
+        </li>
+        <li>
+      <p align="left"><font size="4"><b>SHAKO</b> (USEFUL FOR MILITARY PLANNING)</font></p>
+        </li>
+      </ul>
+      <hr>
+      <p align="center"><b><font size="7" color="#FF0000">THE HEAT LIE</font></b></p>
+      <p align="left"><font size="4">HEAT IS A LIE MADE UP BY SO-CALLED 
+      &quot;SCIENTISTS&quot; WHO CANNOT FATHOM THE FACT THAT IT IS ACTUALLY THE ENERGY 
+      FROM THE SUN. THAT IS WHY WE SENSE IT: WE KNOW WHEN THE SUN IS THERE. 
+      FRAUD-ENTISTS CLAIM THAT TOO MUCH HEAT MAKES YOU SWEAT, BUT IT'S ACTUALLY 
+      THE BAD ENERGIES LEAVING YOUR BODY. BAD ENERGIES ARE LIQUID, GOOD ENERGIES 
+      ARE GASES.</font></p>
+      <p align="left"><font size="4">THE MORE INTENSE THE ENERGY, THE GREATER 
+      THE FEELING AND THE MORE BAD ENERGIES ARE PUSHED OUT OF YOUR BODY. </font></p>
+      <hr>
+      <p align="left"><font size="4">I DISCOVERED THIS TRUTH DURING MY STUDIES 
+      AT MORNINGTON UNIVERSITY. THROUGH RIGOROUS SCIENTIFIC EXPERIMENTS AND 
+      INTENSE DEBATE, I FOUND THE TRUTH OF HAT-WEARING. HOWEVER, THE 
+      EDUCATED/STUPID MASSES AT M.U. WERE SIMPLY TOO TRASHED BY NON-HAT-WEARING 
+      TO GET IT AND I WAS REMOVED.</font></p>
+      <hr>
+      <p align="center"><b><font size="7">IT'S NOT JUST THE HAT THAT MAKES YOU 
+      SMARTER.</font></b></p>
+      <p align="left"><font size="4">THE HAT ALONE WON'T MAKE YOU 
+      HYPER-INTELLIGENT, A COMBINATION OF LIFESTYLE CHANGES ARE REQUIRED TO 
+      REACH PEAK ENLIGHTENMENT. THE HAT IS A SIGNIFICANT CHUNK BUT I HAVE 
+      ADAPTED EVEN MORE LIFESTYLE CHANGES:</font></p>
+      <ol>
+        <li>
+        <p align="left"><font size="4"><b>WEAR DAPPER CLOTHES </b>(BLAZER, 
+        BUTTON UP SHIRT, TROUSERS, SNAKESKIN SHOES, EVEN IN THE HOTTEST OR 
+        COLDEST CLIMATES)</font></li>
+        <li>
+        <p align="left"><font size="4"><b>DRINK WINE OR FRESH RIVER WATER </b>
+        (OTHER LIQUIDS DEGRADE THE MIND)</font></li>
+        <li>
+        <p align="left"><font size="4"><b>NEVER USE &quot;HEATERS&quot; OR ANY &quot;HEATING 
+        ELEMENT&quot;</b> (THEY DISRUPT THE ENERGY FLOW)</font></li>
+        <li>
+        <p align="left"><font size="4"><b>CARRY A CANE</b> (GROUNDS YOU FROM 
+        ELECTRICITY WHICH CAN ALSO DISRUPT ENERGY FLOW. ALSO USEFUL IN LIGHTNING 
+        STRIKES)</font></li>
+        <li>
+        <p align="left"><font size="4"><b>EAT ONLY STEAK, POTATOES AND BREAD</b> 
+        (OTHER FOODS DEGRADE THE BODY)</font></li>
+        <li dir="ltr">
+        <p align="left" dir="ltr"><font size="4"><b>LEARN CURSIVE </b>(THE HAND 
+        MOTIONS PRODUCE BETTER PERFORMANCE IN ACADEMIA)</font></li>
+      </ol>
+      <p align="left"><font size="4">ALL OF THESE ARE ESSENTIAL.</font></p>
+      <hr>
+      <p align="left"><font size="4">THE ONLY WAY I WILL ACCEPT ANY KIND OF 
+      ARGUMENT AGAINST THE HAT TRUTH IS IF YOU ARE ABLE TO DEFEAT ME IN 
+      FISTICUFFS. THE HAT HAS RECYCLED SO MUCH POWER INTO ME TO THE POINT WHERE 
+      I SIMPLY AM TOO POWERFUL TO BE DEFEATED IN HAND-TO-HAND COMBAD. </font></p>
+      <p align="left"><font size="4">IF YOU WANT TO SPREAD THE TRUTH, PRINT THIS 
+      PAGE OUT OR EMAIL ME AT THIS WEBZONE.</font></p>
+      <p align="left"><font size="4">IF YOU WANT TO ARGUE WITH ME ON THESE 
+      POINTS LIKE A DUMB ASS, MEET ME AT THE BACK OF ROBERT'S HOT DOG STAND AT 
+      THE CORNER OF MURAY STREET AND PREPARE FOR FISTICUFFS.</font></p>
+      <p align="center"><img border=0 width=214 height=66
+src="src/img/logo.gif" alt=HATMAN v:shapes="_x0000_s1027"></p>
+    </blockquote>
+    <p>&nbsp;</td>
+  </tr>
+</table>
+
+  </center>
+</div>
+
+</body>
+
+</html>
