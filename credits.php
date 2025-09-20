87fea07d362e69d5bc4793848ca61afb2cc96b60
Linus Kay <linuskay@protonmail.com>
2025-09-20 18:55:00 +1000

    Update credits

--- credits.php
+++ credits.php
@@ -24,17 +24,23 @@
 <body>
     <h1>Credits</h1>
     <p>Virtual Dream created by <a href="https://libus.xyz">Linus Kay</a></p>
-    <p>VD subsists on negative money, but is quite cheap to run. That said, if you enjoy things like this, you can always <a href="https://ko-fi.com/libus_">contributing</a> to hosting costs.</p>
-    <hr>
-    <p>Many animated images are sourced from the GifCities media library, compiled by the Internet Archive</p>
-    <p>Some portrait photos sourced from <a href="https://thispersondoesnotexist">https://thispersondoesnotexist.com/</a></p>
-    <p><b>funktempest</b> Tracks <i>He Who Holds the Zest</i>, <i>Must Learn to Control the Zest</i>, <i>Dominion of the Fist</i>, <i>Evaporate the Nonbelievers</i>, <i>Lavender Tea</i>, and <i>Priests of the Temple of the Fist</i> produced and provided by <a href="https://soundcloud.com/funktempest">Funk Tempest</a>.</p>
-    <p><b>planetxarxax</b> site design initially based on <a href="https://web.archive.org/web/20010211105806/http://www.animetric.com/index.html">archives of animetric.com</a></p>
-    <p><i>Bit</i> sticker character created by Benjamin Grant, re-modeled by Linus Kay</p>
-    <p><b>thebutton</b> created by Heathcorp: <a href="https://github.com/Heathcorp/the-button">https://github.com/Heathcorp/the-button</a></p>
+    <p><b>Images</b></p>
     <p>Virtual Dream logo created by Cecile Richard</p>
-    <p><b>bigger</b> created by brandonhare</p>
-    <hr>
-    <p>See something missing? <a href="mailto:webmaster@virtualdream.live">Let me know</a>!</p>
+    <p>A plethora of gifs sourced from generously compiled online repositories:</p>
+    <ul>
+        <li><a href="https://gifcities.org/">GifCities</a></li>
+        <li><a href="https://sadthemes.tumblr.com/smolpxl">ps1/sadthemes's SMALL PIXELS</a></li>
+    </ul>
+    <p>Portraits, and photobashing elements sourced from:</p>
+    <ul>
+        <li><a href="https://thispersondoesnotexist.com/">ThisPersonDoesNotExist (back in 2021)</a></li>
+        <li><a href="https://unsplash.com">Unsplash</a></li>
+        <li><a href="https://frutigeraeroarchive.org/">The Frutiger Aero Archive</a></li>
+    </ul>
+    <p><b>Sites</b></li>
+        <ul><li><b>funktempest</b> Tracks <i>He Who Holds the Zest</i>, <i>Must Learn to Control the Zest</i>, <i>Dominion of the Fist</i>, <i>Evaporate the Nonbelievers</i>, <i>Lavender Tea</i>, and <i>Priests of the Temple of the Fist</i> produced and provided by <a href="https://soundcloud.com/funktempest">Funk Tempest</a>.</li>
+        <li><b>thebutton</b> created by <a href="https://github.com/Heathcorp/the-button">Heathcorp</a></li>
+        <li><b>bigger</b> created by brandonhare</p>
+    </ul>
 </body>
 </html>
